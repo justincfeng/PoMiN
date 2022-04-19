a147728dcb05ca47b99bb3a34b880fab79707b3a
justin.feng@tecnico.ulisboa.pt <>
2022-04-19 21:39:28 +0100

    Fixed duplicate

--- src/HamPM.jl
+++ src/HamPM.jl
@@ -252,24 +252,24 @@ function dH( d::Int , m::RealVec , Z::RealVec )
     return ForwardDiff.gradient(x->H(d,m,x),Z) + dH3m0( d , m , Z )
 end
 
-# Symplectic operator: Maps output of dH to time derivative of phase space variables
-function Jsympl( Zarg::RealVec )
-    tpfl=typeof(Zarg[1])
-    n2 = length(Zarg)
-    Z = zeros(tpfl,n2)
+## Symplectic operator: Maps output of dH to time derivative of phase space variables
+#function Jsympl( Zarg::RealVec )
+#    tpfl=typeof(Zarg[1])
+#    n2 = length(Zarg)
+#    Z = zeros(tpfl,n2)
 
-    if iseven(n2)
-        n = Int(round(n2 / 2, digits=0))
-        for i=1:n
-            Z[i]    = Zarg[n+i] 
-            Z[n+i]  = - Zarg[i] 
-        end
-
-        return Z
-    else
-        return Z
-    end
-end
+#    if iseven(n2)
+#        n = Int(round(n2 / 2, digits=0))
+#        for i=1:n
+#            Z[i]    = Zarg[n+i] 
+#            Z[n+i]  = - Zarg[i] 
+#        end
+#
+#        return Z
+#    else
+#        return Z
+#    end
+#end
 
 # Right hand side of Hamilton's equations
 FHE = (d,m,z)->Jsympl(dH(d,m,z))
