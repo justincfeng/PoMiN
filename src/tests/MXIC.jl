92727d801e85bcac8e285dfb1774a2ca0b8db166
Justin C. Feng <justin.feng@tecnico.ulisboa.pt>
2023-06-04 21:41:29 -0500

    jcfeng autocommit Sun Jun  4 09:41:29 PM CDT 2023

--- src/tests/MXIC.jl
+++ src/tests/MXIC.jl
@@ -16,27 +16,28 @@ p   = Double64(1/2)
 b   = Double64(6e15)
 d   = Double64(b*10^6)
 
+#-----------------------------------------------------------------------
 function MXICgen(m1,μ,p,b,d;
-                    G=Double64(1),c=Double64(1),
-                    StartTime=Double64(0),MaxTimeStepFactor=Double64(1e5))
+                 G=Double64(1),c=Double64(1),
+                 StartTime=Double64(0),MaxTimeStepFactor=Double64(1e5))
 
     n   = Double64(2)
 
     if m1 == Double64(0.)
         Dur = d/c
-        r1  = d/2
-        r2  = d/2
+        r1  = d/Double64(2)
+        r2  = d/Double64(2)
     else
         v1  = p*c/√((c*m1)^2 + p^2)
         if μ == Double64(0.)
             τ   = d/(v1+c)
-            Dur = 2*τ
+            Dur = Double64(2)*τ
             r1  = v1*τ
             r2  = c*τ
         else
             v2  = p*c/√((μ*c*m1)^2 + p^2)
             τ   = d/(v1+v2)
-            Dur = 2*τ
+            Dur = Double64(2)*τ
             r1  = v1*τ
             r2  = v2*τ  
         end
@@ -47,25 +48,21 @@ function MXICgen(m1,μ,p,b,d;
     E1  = c*√((c*m1)^2 + p^2)
     E2  = c*√((μ*c*m1)^2 + p^2)
 
-    dp  = (2*G*(E1*E2)^2)*
-          (
-            Double64(1.)
-            + ( 1/E1^2 + 1/E2^2 + 4/(E1*E2) )*p^2
-            + p^4/(E1*E2)^2
-          )
-          /
-          (b*p*(E1+E2))
+    dp  = ( (Double64(2)*G*(E1*E2)^2)/(b*p*(E1+E2)) )*
+          ( Double64(1) + 
+            ( Double64(1)/E1^2 + Double64(1)/E2^2 + 
+              Double64(4)/(E1*E2) )*p^2 + p^4/(E1*E2)^2 )
 
-    dpm = 4*G*p^2 / b
+    #dpm = 4*G*p^2 / b
 
     m2  = μ*m1
 
     qx1  = - r1
-    qy1  = - b/2
+    qy1  = - b/Double64(2)
     qz1  = Double64(0.)
 
     qx2  = r2
-    qy2  = b/2
+    qy2  = b/Double64(2)
     qz2  = Double64(0.)
 
     px1  = p
@@ -76,20 +73,33 @@ function MXICgen(m1,μ,p,b,d;
     py2  = Double64(0.)
     pz2  = Double64(0.)
 
-    return (
-    dp , 
-    Parameters((false,1.),(true,0.005),(false,1e-6),(0.,StartTime+Dur),MaxTimeStep) ,
-    [qx1;qy1;qz1;qx2;qy2;qz2;px1;py1;pz1;px2;py2;pz2]
-    )
-end
+    return ( dp , Parameters((false,Double64(1.)),
+                             (true,Double64(0.005)),
+                             (false,Double64(1e-6)),
+                             (Double64(0.),StartTime+Dur),MaxTimeStep) ,
+                    [m1;m2] ,
+                    [qx1;qy1;qz1;qx2;qy2;qz2;px1;py1;pz1;px2;py2;pz2] )
+end #-------------------------------------------------------------------
 
-m1  = 0.0497992;
-μ   = π/4;
-db  = 100000;
-p   = 10*m1;
-b   = 1.110830555874559*10.;
-d   = db*b;
+m1  = Double64(0.0497992);
+μ   = Double64(π/4);
+db  = Double64(100000);
+p   = Double64(10*m1);
+b   = Double64(10*1.1108305558745590335689712446765042841434478759765625);
+d   = Double64(db*b);
 
 mxicd=MXICgen(m1,μ,p,b,d;
                     G=Double64(1),c=Double64(1),
                     StartTime=Double64(0),MaxTimeStepFactor=Double64(1e5))
+
+                    
+hrkintegrator(3, 2, mxicd[4] , x -> pomin.dH_plus_MW(3, mxicd[3], x), δ, no_adapt, mxicd[2].tspan, mxicd[2].iter)
+
+#-----------------------------------------------------------------------
+function MXICmassive(m1,μ,p,b,d;
+    G=Double64(1),c=Double64(1),
+    StartTime=Double64(0),MaxTimeStepFactor=Double64(1e5))
+
+    hrkintegrator(3, 2, Z_init, x -> pomin.dH_plus_MW(3, masses, x), δ, no_adapt, tspan, maxit)
+
+end #-------------------------------------------------------------------
