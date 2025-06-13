af3307a5c369ba0d2cb02a88310bc193b876590c
Justin C. Feng <feng@fzu.cz>
2025-06-13 15:54:12 +0200

    jcfeng autocommit Fri Jun 13 03:54:12 PM CEST 2025

--- src/core/pomin-types.jl
+++ src/core/pomin-types.jl
@@ -1,8 +1,6 @@
 const RealVec{T<:Real} = Array{T}
 const RealMtx{T<:Real} = Array{T,2}
 
-
-
 #   Particle datatype
 struct Particles
     m::RealVec
@@ -38,50 +36,4 @@ function tpnum(tpfl::Type)
                 Float64(4.0),Float64(5.0),Float64(6.0),Float64(7.0),
                 Float64(8.0),Float64(9.0))
     end
-end #-------------------------------------------------------------------
-
-#-----------------------------------------------------------------------
-"""
-    to_phase_tuple(system::Particles)
-
-Convert a Particles object into a tuple of (Z, m, d) suitable for use with FHE.
-
-Arguments:
-- system: A Particles object
-
-Returns:
-- Z: Phase space vector [q1...qN, p1...pN]
-- m: Mass vector
-- d: Number of dimensions (3)
-
-Example:
-```julia
-Z, m, d = to_phase_tuple(system)
-FHE(Z, m, d)  # Ready for Hamilton's equations
-```
-"""
-function to_phase_tuple(system::Particles)
-    # Get number of dimensions from first position vector
-    d = length(system.q[1])
-    N = length(system.m)  # number of particles
-    tpfl = eltype(system.m)  # system's numeric type
-    
-    # Initialize phase space vector
-    Z = zeros(tpfl, 2*N*d)
-    
-    # Fill positions (first half)
-    for i in 1:N
-        for j in 1:d
-            Z[d*(i-1) + j] = system.q[i][j]
-        end
-    end
-    
-    # Fill momenta (second half)
-    for i in 1:N
-        for j in 1:d
-            Z[d*(i-1+N) + j] = system.p[i][j]
-        end
-    end
-    
-    return Z, copy(system.m), d
 end #-------------------------------------------------------------------
--- src/pomin.jl
+++ src/pomin.jl
@@ -40,6 +40,20 @@ include("io/io.jl")                     # I/O routines
 
 # Export public interface
 
+function solve(system::Particles, params::Parameters)
+    m = system.m
+    z = vcat(system.q...,system.p...)
+    
+    if params.jli[1]
+        include("integrators/intjul.jl")
+        return jlintegratorfull(z,HamPM.FHE,m,params.tspan)    
+    elseif params.rkl[1]
+        include("integrators/rk4i.jl")
+        include("integrators/tadap.jl")
+        return rkl_solv(z,HamPM.FHE,m,params.tspan)
+    end
+end #-------------------------------------------------------------------
+
 # Types
 export RealVec, Particles, soln
 
